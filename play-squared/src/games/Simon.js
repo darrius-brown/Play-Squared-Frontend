7523d8a0d68241b9dc22b18fe38da0c1c6f00dcd
darriub15 <darriusabrown@gmail.com>
2022-07-13 20:02:43 -0700

    Flashing square working

--- play-squared/src/games/Simon.js
+++ play-squared/src/games/Simon.js
@@ -3,28 +3,37 @@ import { CreateVariables } from '../styled-components/styles'
 function Simon() { 
     const initialState = {
         boardSize: 16,
-        chosenSquare: 2,
+        chosenSquare: null,
         round: 1,
+        previousSquares: []
         
     }
-
     const [randomSquareState, setFormState] = useState(initialState)
-    const placeRandomSquare = () => {
+    const placeSquare = () => {
         setFormState({
             boardSize: 16,
             chosenSquare: Math.floor(Math.random() * randomSquareState.boardSize),
-            round: randomSquareState.round + 1
+            round: randomSquareState.round + 1,
+            previousSquares: randomSquareState.previousSquares.push(randomSquareState.chosenSquare)
         })
-    }
-    console.log()
+        const timeout = setTimeout(() => {
+        setFormState({
+            boardSize: 16,
+            chosenSquare: null,
+            round: randomSquareState.round + 1,
+            previousSquares: randomSquareState.previousSquares
+        })
+        }, 3000)
+        console.log(randomSquareState.chosenSquare)
+    }   
+    
     return (
         <div>
         <div className='medium-board'>
-            {CreateVariables().map((Item, index) =>  (randomSquareState.chosenSquare === index ? <Item id={index} simon flash/> : <Item id={index} />)
-            
+            {CreateVariables().map((Item, index) =>  (randomSquareState.chosenSquare === index ? <Item id={index} simon /> : <Item id={index} />)
             )}
         </div>
-        <button onClick={placeRandomSquare}>Start Round {randomSquareState.round}</button>
+        <button onClick={placeSquare}>Start Round {randomSquareState.round}</button>
         </div>
     )
 }
--- play-squared/src/styled-components/styles.js
+++ play-squared/src/styled-components/styles.js
@@ -1,18 +1,6 @@
 import styled, { css } from 'styled-components'
-//POTENTIAL DRY CODE
 export function CreateVariables(){
   var squares = [];
-
-  const flashSquare = () => {
-    setTimeout(() => 'background: green', 1000)
-  }
-
-  const test = () => {
-    return 'background: green'
-  }
-
-  
-
   for (var i = 0; i < 16; ++i) {
       squares[i] = styled.div`
       background-color: rgba(212, 209, 209, 0.8);
@@ -21,10 +9,8 @@ export function CreateVariables(){
  
         ${props => props.simon && css`
         background: red;
-          ${flashSquare};
             `}
       `
-      
   }
   console.log(squares)
   return squares
