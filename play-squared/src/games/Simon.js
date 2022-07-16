24f5f521cddc042f3589958744d6ef90e250fce0
darriub15 <darriusabrown@gmail.com>
2022-07-16 12:51:37 -0700

    Issues with for loop

--- play-squared/src/games/Simon.js
+++ play-squared/src/games/Simon.js
@@ -11,40 +11,83 @@ function Simon() {
     const [squareState, setSquareState] = useState(initialState)
     const [squareStateHistory, setSquareStateHistory] = useState([])
 
-    const removeSquare = () => {
-        setTimeout(() => {
-            setSquareState({
-                boardSize: 16,
-                chosenSquare: null,
-                round: squareState.round + 1,
-            })
-        }, 1000)
+    const timeout = (delay, history) => {
+        return new Promise( res => setTimeout(setSquareState({
+            boardSize: 16,
+            chosenSquare: history,
+            round: squareState.round,
+        }), delay ))
     }
-
+    const timeout2 = (delay) => {
+        return new Promise( res => setTimeout(setSquareState({
+            boardSize: 16,
+            chosenSquare: null,
+            round: squareState.round,
+        }), delay ))
+    }
+    const historyPlacement = async (history) => {
+        for(let i = 0; i <=history.length; i++) {
+            console.log('running for loop')
+            await timeout(1000, history[i])
+            await timeout2(1000)
+            // await setTimeout(() => {
+            //     setSquareState({
+            //         boardSize: 16,
+            //         chosenSquare: history[i],
+            //         round: squareState.round,
+            //     })
+            // }, 1000)
+            // await setTimeout(() => {
+            //     setSquareState({
+            //         boardSize: 16,
+            //         chosenSquare: null,
+            //         round: squareState.round,
+            //     })
+            // }, 1000)
+            console.log(history[i] + " should be in array")
+        }
+    }
+    
     const placeSquare = () => {
+        // if(squareStateHistory.length !== 0) {
+        //     historyPlacement(squareStateHistory)
+        // } else {
+        //     console.log('No History!')
+        // }
         const newSquare = Math.floor(Math.random() * squareState.boardSize)
         setSquareState({
-            boardSize: 16,
+            boardSize: squareState.boardSize,
             chosenSquare: newSquare,
             round: squareState.round + 1,
         })
         const newArray = [...squareStateHistory, newSquare]
         setSquareStateHistory(newArray)
-        // changePrevSqrArr()
-        // removeSquare()
+        setTimeout(() => {
+            setSquareState({
+                boardSize: squareState.boardSize,
+                chosenSquare: null,
+                round: squareState.round + 1,
+            })
+        }, 1000)
+        console.log(squareStateHistory)
     }
-    const kingFunction  = () => {
-        setTimeout(placeSquare, 10);
-        setTimeout(removeSquare, 30);
+    const startRound =  () => {
+        let myHistory = historyPlacement(squareStateHistory)
+        console.log(myHistory)
+        if(squareStateHistory.length !== 0) {
+            historyPlacement(squareStateHistory)
+        } else {
+            console.log('No History!')
+        }
+        placeSquare()
     }
-
     return (
         <div>
             <div className='medium-board'>
-                {CreateVariables().map((Item, index) => (squareState.chosenSquare === index ? <Item id={index} simon on /> : <Item id={index} />)
+                {CreateVariables().map((Item, index) => (squareState.chosenSquare === index ? <Item id={index} simon /> : <Item id={index} />)
                 )}
             </div>
-            <button onClick={kingFunction}>Start Round {squareState.round}</button>
+            <button onClick={startRound}>Start Round {squareState.round}</button>
         </div>
     )
 }
